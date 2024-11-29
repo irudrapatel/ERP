4b9c48729948d0f0f6b21954a62e2b3ceb3295ca
Rudra Patel <110285266+irudrapatel@users.noreply.github.com>
2024-11-29 10:48:23 +0530

    Category Image Not Show Issuse

--- server/models/category.model.js
+++ server/models/category.model.js
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    description: { type: String, default: "" },
+    name: { type: String, required: true }, // Required field for category name
+    description: { type: String, default: "" }, // Optional field for description
+    image: { type: String, required: true }, // New field for image URL, marked as required
   },
-  { timestamps: true }
+  { timestamps: true } // Automatically adds createdAt and updatedAt fields
 );
 
+// Use existing model if already created; otherwise, create a new one
 const CategoryModel =
   mongoose.models.category || mongoose.model("category", categorySchema);
 
