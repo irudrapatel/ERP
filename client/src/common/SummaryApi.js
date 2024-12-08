361f14e942db69610df5c85fd1c3da222693fb64
Rudra Patel <110285266+irudrapatel@users.noreply.github.com>
2024-12-08 09:38:43 +0530

    Env Add

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,4 +1,4 @@
-export const baseURL = import.meta.env.VITE_API_URL;
+export const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const SummaryApi = {
     register: { url: '/api/user/register', method: 'post' },
